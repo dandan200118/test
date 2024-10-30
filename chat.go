d4f209c77e7334b9e987a6b76d2662f65d6eca23
Johnny Feng <945684725@qq.com>
2024-10-30 16:20:42 +0800

    Update chat.go

--- chat.go
+++ chat.go
@@ -204,7 +204,7 @@ func (c *Chat) State(ctx context.Context) (int, error) {
 	if len(s.Subscriptions) > 0 {
 		iter := s.Subscriptions[0]
 		value := iter.(map[string]interface{})
-		if service, ok := value["service"]; ok && service.(string) != "adasdskdfj" {
+		if service, ok := value["service"]; ok && service.(string) == "youpro_teams" {
 			logrus.Info("used: you pro") // 无限额度
 			return 200, nil
 		}
